7381b63d16e115fc1b53833c2edebfd082a797cf
Phixion <ms@phixion.org>
2015-12-17 19:20:55 +0100

    fixed formatting

--- heh.py
+++ heh.py
@@ -82,7 +82,7 @@ def beat(bot, trigger):
   pick = choice(requests.get("http://www.reddit.com/r/beatheads.json?limit=100", headers=header).json()["data"]["children"])["data"]
   bot.say("%s" % (pick["url"]))
   
-  @willie.module.commands('kadse')
+@willie.module.commands('kadse')
 def kadse(bot, trigger):
   header =  {"User-Agent": "Willie the bot"}
   pick = choice(requests.get("http://www.reddit.com/r/catgifs.json?limit=100", headers=header).json()["data"]["children"])["data"]
