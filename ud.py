a6edee7904f8bf4a9add34c2c51691d5b41b72e7
vespertine22 <mchlappel@outlook.de>
2016-03-11 20:15:57 +0100

    next attempt

--- ud.py
+++ ud.py
@@ -31,11 +31,8 @@ def get_def(path, num=0):
             thumbsdown = resp['list'][num]['thumbs_down']
             points = str(int(thumbsup) - int(thumbsdown))
             total_nom = len(resp['list'])
-            definition = ""
-            if path == "random":
-                word = resp['list'][num]['word'].encode('utf8')
-                definition = 'Word: ' + str(word)[1:] + ' | '
-            definition = definition + 'Definition: ' + str(item)[1:] + " | Number: " + str(nom) + '/' + str(total_nom) + ' | Points: ' + points + ' (03' + str(thumbsup) + '|05' + str(thumbsdown) + ')'
+            word = resp['list'][num]['word'].encode('utf8')
+            definition = str(word)[1:] + ": " + str(item)[1:] + " | Number: " + str(nom) + '/' + str(total_nom) + ' | Points: ' + points + ' (03' + str(thumbsup) + '|05' + str(thumbsdown) + ')'
         except IndexError:
             definition = ('Definition entry %s does'
                           'not exist for \'%s\'.' % (nom, word))
