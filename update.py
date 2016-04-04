7bd8fc17c5303c4b35ebadff5cb65d61dbcf82a2
Phixion <ms@phixion.org>
2016-04-04 04:54:43 +0200

    removed deprecated stuff

--- update.py
+++ update.py
@@ -4,20 +4,20 @@ update.py - Module to update modules directory
 
 import sys
 import subprocess
-import willie
+import sopel
 
 if sys.version_info >= (2, 7):
-    @willie.module.nickname_commands('updatemodules')
+    @sopel.module.nickname_commands('updatemodules')
     def update(bot, trigger):
         if not trigger.admin:
             return
 
         """Pulls the latest versions of all modules from Git"""
-        proc = subprocess.Popen('cd /home/bot/.willie/modules && /usr/bin/git pull',
+        proc = subprocess.Popen('cd /home/bot/.sopel/modules && /usr/bin/git pull',
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE, shell=True)
         bot.reply(proc.communicate()[0])
 else:
-    @willie.module.nickname_commands('update')
+    @sopel.module.nickname_commands('update')
     def update(bot, trigger):
         bot.say('You need to run me on Python 2.7 to do that.')
